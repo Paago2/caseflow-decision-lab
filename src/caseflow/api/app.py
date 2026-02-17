0c154bd62ba23e437dff4474f585e0cef038d0d5
Paago2 <paago2@yahoo.com>
2026-02-17 00:59:41 -0500

    test: align auth tests with standardized error schema

--- src/caseflow/api/app.py
+++ src/caseflow/api/app.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, FastAPI, Security
 from caseflow.api.routes_ready import router as ready_router
 from caseflow.api.routes_version import router as version_router
 from caseflow.core.auth import require_api_key
+from caseflow.core.errors import install_error_handlers
 from caseflow.core.logging import configure_logging
 from caseflow.core.request_id import install_request_id_middleware
 
@@ -16,6 +17,7 @@ request_id_logger.setLevel(logging.INFO)
 request_id_logger.propagate = True
 
 install_request_id_middleware(app)
+install_error_handlers(app)
 
 protected_router = APIRouter(
     prefix="/protected",
